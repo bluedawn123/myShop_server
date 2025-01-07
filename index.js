340a3b93252641de1d2419ba3d3963dc6cac8566
bluedawn123 <haemilyjh@naver.com>
2025-01-07 20:18:49 +0900

    비밀번호 안보이게

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 // 비밀번호 해싱
 const plainPassword = '123123';
 const hashedPassword = bcrypt.hashSync(plainPassword, 10);
-console.log('Hashed Password:', hashedPassword);
+// console.log('Hashed Password:', hashedPassword);
 
 // Load environment variables from .env file
 dotenv.config();
@@ -52,7 +52,6 @@ const dbQuery = (query, params) => {
 };
 
 // Login endpoint
-// Login endpoint
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
@@ -92,6 +91,28 @@ app.post('/login', async (req, res) => {
     return res.status(500).json({ message: '서버 오류' });
   }
 });
+
+
+app.get('/list', (req, res) => {
+  const sql = "SELECT BOARD_ID, BOARD_TITLE, REGISTER_ID, DATE_FORMAT(REGISTER_DATE , '%Y-%m-%d') AS REGISTER_DATE FROM board";
+  db.query(sql, (err, result) => {
+    if (err) throw err;  
+    res.send(result);
+  })  
+})
+
+app.post('/insert', (req, res) => {
+  //중요
+  //클라이언트단에서 post로 보낸 정보는 req에 들어있고, 이걸 확인해보면
+  //req.body에 각각 나뉘어져 들어있다. 이 부분이 php와 다름
+  let title = req.body.title;
+  let content = req.body.content;
+  const sql = "INSERT INTO board (BOARD_TITLE, BOARD_CONTENT, REGISTER_ID) VALUES (?,?,'admin')";
+  db.query(sql, [title, content], (err, result) => {
+    if (err) throw err;  
+    res.send(result);
+  })  
+})
 
 
 
