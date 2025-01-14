d984bf1b2dc36b58ecb51eabe86942ce037f4f33
bluedawn123 <haemilyjh@naver.com>
2025-01-14 20:11:52 +0900

    // app.use('/api/categories', categoryRoutes)

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const boardRoutes = require('./routes/boardRoutes');
+// const categoryRoutes = require('./routes/categoryRoutes');
 
 dotenv.config();
 
 const app = express();
 const port = 8000;
 
+
 app.use(express.json());
 app.use(cors());
 
 app.use('/auth', authRoutes);
 app.use('/board', boardRoutes);
+// app.use('/api/categories', categoryRoutes)
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
